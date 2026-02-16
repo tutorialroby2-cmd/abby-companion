045d0d4b8ebb800a4e13f5be3166d81dbde3b700
tutorialroby2-cmd <tutorialroby2@gmail.com>
2026-02-16 14:25:07 +0100

    Update MainActivity.kt

--- app/src/main/java/org/nehuatl/sample/MainActivity.kt
+++ app/src/main/java/org/nehuatl/sample/MainActivity.kt
@@ -1,58 +1,28 @@
 package org.nehuatl.sample
 
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.compose.viewModel
-import org.nehuatl.sample.ui.theme.KotlinLlamaCppTheme
 
 class MainActivity : ComponentActivity() {
-
-    private var modelPath by mutableStateOf<String?>(null)
-
-    private val filePickerLauncher = registerForActivityResult(
-        ActivityResultContracts.OpenDocument()
-    ) { uri ->
-        uri?.let {
-            contentResolver.takePersistableUriPermission(
-                it,
-                Intent.FLAG_GRANT_READ_URI_PERMISSION
-            )
-            modelPath = it.toString()
-        }
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        enableEdgeToEdge()
         setContent {
-            KotlinLlamaCppTheme {
-                val viewModel: MainViewModel = viewModel{
-                    MainViewModel(contentResolver)
+            val vm: MainViewModel = viewModel(
+                factory = object : ViewModelProvider.Factory {
+                    @Suppress("UNCHECKED_CAST")
+                    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                        return MainViewModel(contentResolver) as T
+                    }
                 }
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    ChatScreen(
-                        modifier = Modifier.padding(innerPadding),
-                        viewModel = viewModel,
-                        currentModelPath = modelPath,
-                        onPickModel = {
-                            filePickerLauncher.launch(arrayOf("*/*"))
-                        }
-                    )
-                }
-            }
+            )
+
+            ChatScreen(viewModel = vm)
         }
     }
 }
